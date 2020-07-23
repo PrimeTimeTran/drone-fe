ce1b45e158b427a1dd004765a1d0c75e8400cd28
Loi Van Tran <datloiboi@gmail.com>
2020-07-23 22:00:58 +0700

    Refactor Chart.js

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+import { produceChartData } from "./utils";
+
+const Chart = ({ data: quizData }) => {
+  console.log(quizData)
+  const data = produceChartData(quizData);
+  return (
+    <div className="chart">
+      <Bar data={data} options={{}} />
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Chart/utils.js
@@ -0,0 +1,37 @@
+import moment from "moment";
+
+export const produceChartData = (data) => {
+  const labels = [];
+  const scoreData = [];
+  data.forEach(quiz => {
+    scoreData.push(quiz.score);
+    labels.push(moment(quiz.createdAt).format("LL"));
+  })
+
+  return {
+    labels,
+    datasets: [
+      {
+        label: "% Score of Quiz Taken",
+        data: scoreData,
+        backgroundColor: [
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+        ],
+        borderColor: [
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+};
