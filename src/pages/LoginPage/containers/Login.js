4a87504ea9ba882bdfd439d4b91938f484926b3d
Loi Van Tran <datloiboi@gmail.com>
2020-07-27 11:31:09 +0700

    Refactor login & register

--- src/pages/LoginPage/containers/Login.js
+++ src/pages/LoginPage/containers/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
@@ -7,19 +7,14 @@ import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
 import { login } from "./UserFunctions";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      verifiedCaptcha: false,
-    };
+const Login = (props) => {
+  const [captcha, setCaptcha] = useState(false);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    this.onChange = this.onChange.bind(this);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const container = document.getElementById("recaptcha");
     if (container) {
       setTimeout(() => {
@@ -30,97 +25,86 @@ class Login extends Component {
         }
       }, 100);
     }
-  }
+  }, []);
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    console.log({ la: this.state.verifiedCaptcha });
-
-    if (this.state.verifiedCaptcha) {
-      const user = {
-        email: this.state.email,
-        password: this.state.password,
-      };
+    if (captcha) {
       login(user).then((res) => {
         if (res) {
-          this.props.checkUser();
-          this.props.history.push("/home");
+          props.checkUser();
+          props.history.push("/home");
         }
       });
     }
   };
 
-  onRecaptcha = async (e) => {
-    this.setState({ verifiedCaptcha: true });
-  };
-
-  render() {
-    console.log({ state: this.state });
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Card>
-              <Card.Header>
-                {this.props.renderBack()}
-              </Card.Header>
-              <Card.Body>
-                <Card.Title>Log In</Card.Title>
-                <Card.Text>
-                  Get back to studying to get your certification and change your life with drones.
-                </Card.Text>
-                <form noValidate onSubmit={this.onSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter"
-                      className="form-control"
-                      value={this.state.email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      className="form-control"
-                      onChange={this.onChange}
-                      placeholder="Enter password"
-                      value={this.state.password}
-                    />
-                  </div>
-                  <ReCAPTCHA
-                    id="recaptcha"
-                    className="my-3"
-                    onChange={this.onRecaptcha}
-                    sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header>{props.renderBack()}</Card.Header>
+            <Card.Body>
+              <Card.Title>Log In</Card.Title>
+              <Card.Text>
+                Get back to studying to get your certification and change your
+                life with drones.
+              </Card.Text>
+              <form noValidate onSubmit={onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={user.email}
+                    placeholder="Enter"
+                    onChange={onChange}
+                    className="form-control"
                   />
-                  <Row>
-                    <Col className="d-flex flex-column">
-                      <Button variant="primary" type="submit">
-                        Login
-                      </Button>
-                      <Link to="/reset-password" className="text-right">
-                        Forgot Password?
-                      </Link>
-                    </Col>
-                  </Row>
-                </form>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={onChange}
+                    value={user.password}
+                    className="form-control"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <ReCAPTCHA
+                  id="recaptcha"
+                  className="my-3"
+                  onChange={() => setCaptcha(true)}
+                  sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+                />
+                <Row>
+                  <Col className="d-flex flex-column">
+                    <Button variant="primary" type="submit">
+                      Login
+                    </Button>
+                    <Link to="/reset-password" className="text-right">
+                      Forgot Password?
+                    </Link>
+                  </Col>
+                </Row>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default withRouter(Login);
--- src/pages/LoginPage/containers/Register.js
+++ src/pages/LoginPage/containers/Register.js
@@ -1,118 +1,106 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
 import { register } from "./UserFunctions";
 
-class Register extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: "",
-      password: "",
-      last_name: "",
-      first_name: "",
-      verifiedCaptcha: false,
-    };
-  }
+const Register = (props) => {
+  const [captcha, setCaptcha] = useState(false);
+  const [user, setUser] = useState({
+    password: "",
+    last_name: "",
+    first_name: "",
+  });
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  onSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
-    const user = {
-      email: this.state.email,
-      password: this.state.password,
-      first_name: this.state.first_name,
-      last_name: this.state.last_name,
-    };
-    if (this.state.verifiedCaptcha) {
+    if (captcha) {
       register(user).then((res) => {
-        this.props.checkUser();
+        props.checkUser();
         window.location.replace(process.env.REACT_APP_SITE_URL);
       });
     }
-  }
-
-  onRecaptcha = async (e) => {
-    this.setState({ verifiedCaptcha: true });
   };
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Card>
-              <Card.Header>{this.props.renderBack()}</Card.Header>
-              <Card.Body>
-                <Card.Title>Register</Card.Title>
-                <Card.Text>
-                Get back to studying to get your certification and change your life with drones.
-                </Card.Text>
-                <form noValidate onSubmit={(e) => this.onSubmit(e)}>
-                  <div className="form-group">
-                    <label htmlFor="first_name">First Name</label>
-                    <input
-                      type="text"
-                      name="first_name"
-                      className="form-control"
-                      onChange={this.onChange}
-                      placeholder="Enter First Name"
-                      value={this.state.first_name}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="last_name">Last Name</label>
-                    <input
-                      type="email"
-                      name="last_name"
-                      className="form-control"
-                      onChange={this.onChange}
-                      placeholder="Enter Last name"
-                      value={this.state.last_name}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter"
-                      className="form-control"
-                      value={this.state.email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      className="form-control"
-                      onChange={this.onChange}
-                      placeholder="Enter password"
-                      value={this.state.password}
-                    />
-                  </div>
-                  <Button variant="primary" type="submit">
-                    Register
-                  </Button>
-                </form>
-              </Card.Body>
-            </Card>
-            <ReCAPTCHA
-              onChange={this.onRecaptcha}
-              sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
-            />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header>{props.renderBack()}</Card.Header>
+            <Card.Body>
+              <Card.Title>Register</Card.Title>
+              <Card.Text>
+                Get back to studying to get your certification and change your
+                life with drones.
+              </Card.Text>
+              <form noValidate onSubmit={onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="first_name">First Name</label>
+                  <input
+                    type="text"
+                    name="first_name"
+                    onChange={onChange}
+                    value={user.first_name}
+                    className="form-control"
+                    placeholder="Enter First Name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="last_name">Last Name</label>
+                  <input
+                    type="email"
+                    name="last_name"
+                    onChange={onChange}
+                    value={user.last_name}
+                    className="form-control"
+                    placeholder="Enter Last name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email Address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={user.email}
+                    placeholder="Enter"
+                    onChange={onChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={onChange}
+                    value={user.password}
+                    className="form-control"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <Button variant="primary" type="submit">
+                  Register
+                </Button>
+              </form>
+            </Card.Body>
+          </Card>
+          <ReCAPTCHA
+            onChange={() => setCaptcha(true)}
+            sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default withRouter(Register);
