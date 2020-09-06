4a21cf67e7f7be0d3f6263d61cbf3f236575bdac
Loi Van Tran <datloiboi@gmail.com>
2020-09-06 09:49:41 +0700

    Fix order

--- src/components/Chart/utils.js
+++ src/components/Chart/utils.js
@@ -3,22 +3,23 @@ import moment from "moment";
 export const produceChartData = (data) => {
   const labels = [];
   const scoreData = [];
-  const colors = []
-  Array.isArray(data) && data.forEach(quiz => {
-    colors.push("#333399")
-    scoreData.push(quiz.score);
-    labels.push(moment(quiz.createdAt).format("LL"));
-  })
+  const colors = [];
+  Array.isArray(data) &&
+    data.forEach((quiz) => {
+      colors.push("#333399");
+      scoreData.push(quiz.score);
+      labels.push(moment(quiz.createdAt).format("LL"));
+    });
 
   return {
     labels,
     datasets: [
       {
-        label: "% Score of Quiz Taken",
-        data: scoreData,
-        backgroundColor: colors,
-        borderColor: colors,
         borderWidth: 1,
+        data: scoreData,
+        borderColor: colors,
+        backgroundColor: colors,
+        label: "% Score of Quiz Taken",
       },
     ],
   };
